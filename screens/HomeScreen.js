f91d36f1b7456726843631c92cba5d89ed2dd7d8
dhabek <dunja.habek@hotmail.com>
2021-12-20 09:07:40 +0100

    vjezba10

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,40 @@
 import React from "react";
 import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
-
+import * as Google from "expo-google-app-auth"
 export function HomeScreen({ route, navigation }) {
-  function handleSettingsPress() {
-    navigation.navigate("Settings");
+
+  const handleGoogleSignIn = () => {
+    const config = {
+      iosClientId: "421591619646-h9ogh6jodh2q2fot8s7bj4ngrg169pu5.apps.googleusercontent.com",
+      scopes: ["profile", "email"]
+    }
+    Google.logInAsync(config).then((result) => {
+      const {type, user} = result;
+      if (type == "success") {
+        const {email, name} = user;
+        console.log("Sign in successfull")
+        setTimeout(
+          () => navigation.navigate("Settings", {email, name}), 1000
+        ) 
+      } else {
+        console.log("Sign in failed")
+      }
+    }).catch((error) => {
+      console.log("error")
+    })
   }
+
+
 
   return (
     <View style={styles.screen}>
       <Text>The Home Screen!</Text>
       <Button
-        title="Go to the Settings screen!"
-        onPress={handleSettingsPress}
+        title="Signup"
+        onPress={handleGoogleSignIn}
       />
+      {/* iOS client ID
+       421591619646-h9ogh6jodh2q2fot8s7bj4ngrg169pu5.apps.googleusercontent.com */}
     </View>
   );
 }
